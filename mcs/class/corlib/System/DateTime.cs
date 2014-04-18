9ca95ed3aff98f48f1bbd3e2e0717c0083a070b2
davidpodhola <david.podhola@naseukoly.cz>
2014-04-18 07:58:27 +0200

    another quick hack to solve the EWS datetime format issue

--- mcs/class/corlib/System/DateTime.cs
+++ mcs/class/corlib/System/DateTime.cs
@@ -886,6 +886,10 @@ namespace System
                 if (s[4] == '-' && s[7] == '-' && s[10] == '+' && s[13] == ':' && s.StartsWith("20") && s.EndsWith("+02:00") )
                 {
                     s = s.Replace("+02:00", "");
+
+                    string[] parts = s.Split('-');
+                    DateTime result = new DateTime(int.Parse(parts[0]), int.Parse(parts[1]), int.Parse(parts[2]));
+                    s = result.ToString();
                 }
             }
 
